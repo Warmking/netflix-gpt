1650f53f0cdbd14855516caf41fa51006816bfd0
mohanDev <kandulamohanreddyking@gmail.com>
2024-02-12 16:51:00 +0530

    cra & config tailwinf

---
+++ README.md
@@ -0,0 +1,4 @@
+# Netflix-Gpt
+- created react app using vite 
+  - npm create vite@latest Netflix-Gpt -- --template react
+- Configure Tailwind CSS in the project
---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <p className="text-3xl text-red-600 bg-black p-2 text-center">
+        NetFlix integrated with the GPT
+      </p>
+    </>
+  )
+}
+
+export default App
