2038a9593e7fd4bf732a7ebadab47e679a7cb30d
Mohan Reddy <kandulamohanreddyking@gmail.com>
2024-02-28 12:16:16 +0530

    form update

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -3,8 +3,9 @@ import Header from "./Header";
 
 const LoginPage = () => {
   const [signIn, setSignIn] = useState(true);
-  const handleButtonClick = () => {
-    setSignIn(!signIn)
+  const handleButtonClick = (e) => {
+    e.preventDefault()
+    setSignIn(!signIn);
   };
 
   return (
@@ -16,55 +17,58 @@ const LoginPage = () => {
           alt="background-img"
         />
       </div>
-      <div className="p-4 border bg-black opacity-90 w-96 absolute top-[25%] right-[40%] border-black rounded-2xl">
-        <div className="m-2">
-          <h1 className="text-4xl p-1 mx-2  my-4 z-1 text-white font-bold ">
-            {signIn ?'Sign In': 'Sign Up'}
-          </h1>
-          {
-            !signIn && <input
+      <form className="p-4 border bg-black opacity-90 w-96 absolute top-[25%] right-[40%] border-black rounded-2xl">
+        <h1 className="text-4xl p-1 mx-2  my-4 z-1 text-white font-bold ">
+          {signIn ? "Sign In" : "Sign Up"}
+        </h1>
+        {!signIn && (
+          <input
             className="block p-4 mx-2  my-4 border border-gray-100 rounded-lg w-80 bg-black text-white outline-none"
             placeholder="Full Name"
             type="text"
-          /> 
-          }
-          <input
-            className="block p-4 mx-2  my-4 border border-gray-100 rounded-lg w-80 bg-black text-white outline-none"
-            placeholder="Email or Phone Number"
-            type="text"
           />
-          <input
-            className="block p-4 mx-2  my-4 border border-gray-100 rounded-lg w-80 bg-black text-white outline-none"
-            type="password"
-            placeholder="Password"
-          />
-          <button className="bg-red-500 text-white block py-2 px-4 mx-2  my-4 w-80 rounded-lg text-center hover:bg-red-700 bg-opacity-100 relative z-10">
-            Sign In
-          </button>
-          <p className="text-white text-center pb-6">Forgot password?</p>
-          <p className="text-white">
-            <check></check> Remember Me
-          </p>
-          {
-            signIn ?<> <p className="text-white inline">New to Netflix? </p>
-          <button
-            className="text-white font-bold hover:underline"
-            onClick={handleButtonClick}
-          >
-            Sign up now.
-          </button>
-          </> :  <> <p className="text-white inline">Alrady have one? </p>
-          <button
-            className="text-white font-bold hover:underline"
-            onClick={handleButtonClick}
-          >
-            Sign In now.
-          </button>
+        )}
+        <input
+          className="block p-4 mx-2  my-4 border border-gray-100 rounded-lg w-80 bg-black text-white outline-none"
+          placeholder="Email or Phone Number"
+          type="text"
+        />
+        <input
+          className="block p-4 mx-2  my-4 border border-gray-100 rounded-lg w-80 bg-black text-white outline-none"
+          type="password"
+          placeholder="Password"
+        />
+        <button className="bg-red-500 text-white block py-2 px-4 mx-2  my-4 w-80 rounded-lg text-center hover:bg-red-700 bg-opacity-100 relative z-10">
+          Sign In
+        </button>
+        <p className="text-white text-center pb-6">Forgot password?</p>
+        <p className="text-white">
+          <check></check> Remember Me
+        </p>
+        {signIn ? (
+          <>
+            {" "}
+            <p className="text-white inline">New to Netflix? </p>
+            <button
+              className="text-white font-bold hover:underline"
+              onClick={handleButtonClick}
+            >
+              Sign up now.
+            </button>
           </>
-          }
-          
-        </div>
-      </div>
+        ) : (
+          <>
+            {" "}
+            <p className="text-white inline">Alrady have one? </p>
+            <button
+              className="text-white font-bold hover:underline"
+              onClick={handleButtonClick}
+            >
+              Sign In now.
+            </button>
+          </>
+        )}
+      </form>
     </div>
   );
 };
